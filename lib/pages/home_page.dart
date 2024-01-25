478fe20ffa91e0d51a59bf1e1ba8dcadb23e2e66
Charan <charankulal0241@gmail.com>
2024-01-25 23:18:56 +0530

    Created the topbar containing menu icon, logo and search icon.

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import "package:flutter/material.dart";
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: _mainColumn(),
+        top: true,
+        left: false,
+        right: false,
+        bottom: true,
+        minimum: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.05,
+            0, MediaQuery.of(context).size.width * 0.05, 0),
+      ),
+    );
+  }
+
+  Widget _mainColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.menu,
+              color: Colors.black87,
+              size: 35,
+            ),
+            Container(
+              height: 39,
+              width: 144,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('../../assets/images/logo_discover.png'),
+                ),
+              ),
+            ),
+            Icon(
+              Icons.search,
+              color: Colors.black87,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
