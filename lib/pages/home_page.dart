7b2df04268d7333a00a13ed8d0672eaf393c701c
Charan <charankulal0241@gmail.com>
2024-01-26 13:02:45 +0530

    Implemented the location bar in the application.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, prefer_final_fields, unused_field, no_leading_underscores_for_local_identifiers
 
 import "package:flutter/material.dart";
 
@@ -10,22 +10,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> _locations = ['Moscow', 'Japan', 'Delhi', 'London'];
+  int _activeLocation = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: _mainColumn(),
         top: true,
         left: false,
         right: false,
         bottom: true,
         minimum: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.05,
             0, MediaQuery.of(context).size.width * 0.05, 0),
+        child: _mainColumn(context),
       ),
     );
   }
 
-  Widget _mainColumn() {
+  Widget _mainColumn(BuildContext _context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       mainAxisSize: MainAxisSize.max,
@@ -57,7 +60,56 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
+        Padding(
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(_context).size.height * 0.05,
+          ),
+          child: _locationBar(_context),
+        ),
       ],
+    );
+  }
+
+  Widget _locationBar(BuildContext _context) {
+    return Container(
+      height: MediaQuery.of(_context).size.height * 0.065,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(69, 69, 69, 1.0),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: _locations.map(
+          (l) {
+            bool _isActive = _locations[_activeLocation] == l ? true : false;
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  l,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'Montserrat',
+                    color: _isActive ? Colors.white : Colors.white54,
+                  ),
+                ),
+                _isActive
+                    ? Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.redAccent,
+                        ),
+                        height: 5,
+                        width: 50,
+                      )
+                    : Container(),
+              ],
+            );
+          },
+        ).toList(),
+      ),
     );
   }
 }
